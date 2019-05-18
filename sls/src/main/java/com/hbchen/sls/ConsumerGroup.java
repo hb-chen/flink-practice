5fb66e15846a643abb7ff0dccf81482bd9bb3880
Hobo86 <hobo.chb@gmail.com>
2019-05-18 22:45:01 +0800

    添加CheckPoint以及SLS ConsumerGroup的CheckPoint重置

---
+++ sls/src/main/java/com/hbchen/sls/ConsumerGroup.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2019. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.hbchen.sls;
+
+import com.aliyun.openservices.log.Client;
+import com.aliyun.openservices.log.common.Shard;
+import com.aliyun.openservices.log.request.ListShardRequest;
+import com.aliyun.openservices.log.response.ListShardResponse;
+import org.apache.log4j.Logger;
+
+import java.sql.Timestamp;
+
+import com.hbchen.sls.config.Config;
+
+/**
+ * SLS ConsumerGroup操作
+ *
+ * @author hbchen.com
+ */
+public class ConsumerGroup {
+
+	private static final Logger LOG = org.apache.log4j.Logger.getLogger(ConsumerGroup.class);
+
+	public static void main(String[] args) throws Exception {
+		updateCheckpoint();
+	}
+
+	/**
+	 * 重置ConsumerGroup的CheckPoint时间点
+	 * https://help.aliyun.com/document_detail/28998.html
+	 *
+	 * @throws Exception
+	 */
+	public static void updateCheckpoint() throws Exception {
+		Config config = SlsAnalysis.getConfig();
+		String host = config.getSls().getEndpoint();
+		String accessId = config.getSls().getAk();
+		String accessKey = config.getSls().getSk();
+		String project = config.getSls().getProject();
+		String logStore = config.getSls().getLogStore();
+		String consumerGroup = "sls-analysis";
+
+		Client client = new Client(host, accessId, accessKey);
+		long timestamp = Timestamp.valueOf("2019-05-18 00:00:00").getTime() / 1000;
+		ListShardResponse response = client.ListShard(new ListShardRequest(project, logStore));
+		for (Shard shard : response.GetShards()) {
+			int shardId = 15;
+			shard.GetShardId();
+			LOG.info("shared id: " + shardId);
+			String cursor = client.GetCursor(project, logStore, shardId, timestamp).GetCursor();
+			client.UpdateCheckPoint(project, logStore, consumerGroup, shardId, cursor);
+			break;
+		}
+	}
+}
--- sls/src/main/java/com/hbchen/sls/SlsAnalysis.java
+++ sls/src/main/java/com/hbchen/sls/SlsAnalysis.java
@@ -4,8 +4,10 @@ import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.RuntimeContext;
 import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.AssignerWithPunctuatedWatermarks;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
@@ -51,6 +53,8 @@ import com.hbchen.sls.util.AccessLogAnalysis;
 
 /**
  * SLS日志分析.
+ *
+ * @author hbchen.com
  */
 public class SlsAnalysis {
 
@@ -81,7 +85,9 @@ public class SlsAnalysis {
 		// Consts.LOG_END_CURSOR： 表示从shard的尾开始，也就是从shard中最新的数据开始消费。
 		// Consts.LOG_FROM_CHECKPOINT：表示从某个特定的ConsumerGroup中保存的Checkpoint开始消费，通过ConfigConstants.LOG_CONSUMERGROUP指定具体的ConsumerGroup。
 		// UnixTimestamp： 一个整型数值的字符串，用1970-01-01到现在的秒数表示， 含义是消费shard中这个时间点之后的数据。
-		configProps.put(ConfigConstants.LOG_CONSUMER_BEGIN_POSITION, "" + (System.currentTimeMillis() / 1000L));
+		// configProps.put(ConfigConstants.LOG_CONSUMER_BEGIN_POSITION, "" + (System.currentTimeMillis() / 1000L));
+		configProps.put(ConfigConstants.LOG_CONSUMER_BEGIN_POSITION, Consts.LOG_FROM_CHECKPOINT);
+		configProps.put(ConfigConstants.LOG_CONSUMERGROUP, "sls-analysis");
 		// 设置日志拉取时间间隔及每次调用拉取的日志数量
 		configProps.put(ConfigConstants.LOG_FETCH_DATA_INTERVAL_MILLIS, "1000");
 		configProps.put(ConfigConstants.LOG_MAX_NUMBER_PER_FETCH, "100");
@@ -97,9 +103,11 @@ public class SlsAnalysis {
 		env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
 		// 开启flink exactly once语义
-		// env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+		env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
 		// 每5s保存一次checkpoint
-		// env.enableCheckpointing(5000);
+		env.enableCheckpointing(5000);
+		// 作业Cancel是否删除CheckPoint
+		env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
 
 		// SLS批量日志展开
 		DataStream<Tuple4<AccessLogAnalysis, Integer, Long, Long>> flatStream = logStream.flatMap(new FlatMapFunction<RawLogGroupList, Tuple4<AccessLogAnalysis, Integer, Long, Long>>() {
@@ -158,10 +166,7 @@ public class SlsAnalysis {
 					return previousElementTimestamp;
 				}
 			}
-		})
-				.keyBy(0)
-				.timeWindow(Time.seconds(60))
-				.sum(1);
+		}).keyBy(0).timeWindow(Time.seconds(60)).sum(1);
 
 		if (false) {
 			result.print();
@@ -208,7 +213,7 @@ public class SlsAnalysis {
 					@Override
 					public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
 						CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-						credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(config.getEs().getUsername(), esPassword));
+						credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(esUsername, esPassword));
 						return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
 					}
 				});
@@ -221,7 +226,7 @@ public class SlsAnalysis {
 		env.execute();
 	}
 
-	private static Config getConfig() {
+	public static Config getConfig() {
 		Constructor constructor = new Constructor(Config.class);
 		org.yaml.snakeyaml.Yaml yaml = new org.yaml.snakeyaml.Yaml(constructor);
 
